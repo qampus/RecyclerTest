501ca7af788900d08e4c790ce5314a8e330811cb
bagus duta prasetya <bagusdutaprasetya@gmail.com>
2017-10-29 16:01:07 +0700

    tambahan
    comman

--- app/src/main/java/com/example/d2a/recyclertest/MainActivity.java
+++ app/src/main/java/com/example/d2a/recyclertest/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+//tambahan duta
 
 import com.example.d2a.recyclertest.Adapter.MyOwn;
 
